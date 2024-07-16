47e329589d461de8097cba071dfcdfce6e5282d7
jumpman10 <jpmoyano10@gmail.com>
2024-07-16 04:23:30 -0300

    'upload'

--- src/core/constants/index.ts
+++ src/core/constants/index.ts
@@ -3,3 +3,4 @@ export const DEVELOPMENT = 'development';
 export const TEST = 'test';
 export const USER_REPOSITORY = 'USER_REPOSITORY';
 export const TASK_REPOSITORY = 'TASK_REPOSITORY';
+export const PRODUCTION = 'production';
